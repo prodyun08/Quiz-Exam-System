7b3fcd4608540186808b2979a63662c74ee4704c
Prodyun Biswas <92980967+prodyun08@users.noreply.github.com>
2024-06-05 00:09:38 +0530

    Add files via upload

---
+++ Lan Base Exam System/src/quiz/application/Rules.java
@@ -0,0 +1,70 @@
+package quiz.application;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Rules extends JFrame implements ActionListener{
+
+    String name;
+    JButton start, back;
+    
+    Rules(String name) {
+        this.name = name;
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        JLabel heading = new JLabel("Welcome " + name + " to Simple Minds");
+        heading.setBounds(50, 20, 700, 30);
+        heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
+        heading.setForeground(new Color(30, 144, 254));
+        add(heading);
+        
+        JLabel rules = new JLabel();
+        rules.setBounds(20, 90, 700, 350);
+        rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
+        rules.setText(
+            "<html>"+ 
+                "1. Time Limits: Clearly define and enforce time limits for each exam or section." + "<br><br>" +
+                "2. Permissible Materials: Specify what materials (e.g., calculators, notes) are allowed and disallowed." + "<br><br>" +
+                "3. Timed Sections: Break the exam into timed sections, preventing students from moving back and forth between sections." + "<br><br>" +
+                "4. Device Requirements: Specify the technical requirements, such as operating system, browser type, and necessary software/plugins." + "<br><br>" +
+                "5. Data Protection: Ensure that all personal and exam data are encrypted and stored securely." + "<br><br>" +
+                "6. Review Period: Allow a period for students to review their answers before final submission if applicable." + "<br><br>" +
+            "<html>"
+        );
+        add(rules);
+        
+        back = new JButton("Back");
+        back.setBounds(250, 500, 100, 30);
+        back.setBackground(new Color(30, 144, 254));
+        back.setForeground(Color.WHITE);
+        back.addActionListener(this);
+        add(back);
+        
+        start = new JButton("Start");
+        start.setBounds(400, 500, 100, 30);
+        start.setBackground(new Color(30, 144, 254));
+        start.setForeground(Color.WHITE);
+        start.addActionListener(this);
+        add(start);
+        
+        setSize(800, 650);
+        setLocation(350, 100);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == start) {
+            setVisible(false);
+            new Quiz(name);
+        } else {
+            setVisible(false);
+            new Login();
+        }
+    }
+    
+    public static void main(String[] args) {
+        new Rules("User");
+    }
+}
